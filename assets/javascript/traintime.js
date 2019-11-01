73514bb41cba1fa680832a123593015b8ded6b80
Mike Jed <jedmike@gmail.com>
2019-11-01 14:43:56 -0400

    HTML update, JS update

---
+++ assets/javascript/traintime.js
@@ -0,0 +1,19 @@
+alert("ALLGOOD");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCoryH91NBegczmhVDYYvzAtY4JTrcNgOY",
+    authDomain: "traintime-14910.firebaseapp.com",
+    databaseURL: "https://traintime-14910.firebaseio.com",
+    projectId: "traintime-14910",
+    storageBucket: "traintime-14910.appspot.com",
+    messagingSenderId: "821094807579",
+    appId: "1:821094807579:web:e403629d6148f24c6da0be"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database()
+database.ref().push({
+    message: "Hello World"
+
+})
