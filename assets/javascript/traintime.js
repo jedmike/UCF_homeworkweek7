06e3c1997fe71ff4d1fdfbd65d048cb10d0de5d1
Mike Jed <jedmike@gmail.com>
2019-11-11 13:27:19 -0500

    everything working except the calculations

--- assets/javascript/traintime.js
+++ assets/javascript/traintime.js
@@ -5,13 +5,18 @@ var currenttime = today.getHours() + ":" + today.getMinutes();
 var currentHr = today.getHours();
 var currentMin = today.getMinutes();
 var currentTime = moment();
+var initialTrTime = '';
+var nextArrivalTime = "";
 console.log(currentTime);
 console.log(currenttime);
 console.log(currentMin);
-var trNam = "";
-var trdest = "";
-var trfreq = "";
-var trFirstT = "";
+var trName = "";
+var trDestination = "";
+var trFrequency = "";
+var trFistTrainTOD = "";
+var lastArrived = "";
+var minAway = "";
+var swap = "";
 
 
 // Initialize Firebase
@@ -33,17 +38,17 @@ var database = firebase.database();
 $("#addTrain-btn").on("click", function(event) {
     event.preventDefault();
 
-    var trNam = $("#trainName-input").val().trim();
-    var trdest = $("#destination-input").val().trim();
-    var trfreq = $("#frequency-input").val();
-    var trFirstT = $("#firstTime-input").val().trim();
+    var trName = $("#trainName-input").val().trim();
+    var trDestination = $("#destination-input").val().trim();
+    var trFrequency = $("#frequency-input").val();
+    var trFistTrainTOD = $("#firstTime-input").val()
 
     // Object to hole each new train entry
     var newTrain = {
-        name: trNam,
-        destination: trdest,
-        frequency: trfreq,
-        firstTime: trFirstT
+        name: trName,
+        destination: trDestination,
+        frequency: trFrequency,
+        firstTime: trFistTrainTOD
     };
 
 
@@ -51,12 +56,75 @@ $("#addTrain-btn").on("click", function(event) {
     database.ref().push(newTrain);
 
     // // Logs everything to console
-    // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-    // console.log(newTrain.name);
-    // console.log(newTrain.destination);
-    // console.log(newTrain.freqquency);
-    // console.log(newTrain.firstTime);
-    // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.frequency);
+    console.log(newTrain.firstTime);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    alert("Train successfully added");
+    console.loc("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    console.log("buton clicked");
+    console.loc("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+
+
+    //  Clears all of the text-boxes
+    $("#trainName-input").val("");
+    $("#destination-input").val("");
+    $("#frequency-input").val("");
+    $("#firstTime-inputt").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    console.log(childSnapshot.val());
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+    // Store everything into a variable.
+    var trName = childSnapshot.val().name
+    var trDestination = childSnapshot.val().destination;
+    var trFrequency = childSnapshot.val().frequency;
+    var trFistTrainTOD = childSnapshot.val().firstTime;
+
+
+
+    console.log("::::::::::::::::::::::::::::::::::::");
+    console.log("new train name:  " + trName);
+    console.log("new train destination:  " + trDestination);
+    console.log("new train frequency:  " + trFrequency);
+    console.log("new train starts service at:  " + trFistTrainTOD);
+    console.log("::::::::::::::::::::::::::::::::::::");
+
+    //Calculate train timint
+    var initialTrTime = moment(trFistTrainTOD, "HH:mm ");
+    // var nextArrivalTime = currentTime.diff(initialTrTime, "minutess");
+    var nextArrivalTime = initialTrTime.diff(currentTime, "minutes");
+    var lastArrived = nextArrivalTime % trFrequency;
+    console.log("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
+    var minAway = trFrequency - lastArrived;
+    console.log("Moment initial train time:" + initialTrTime);
+    console.log("System time:" + currenttime);
+    console.log("cuttern time:" + currentTime);
+    console.log("initialTime =" + initialTrTime);
+    console.log("nextArrivalTime = " + nextArrivalTime);
+    console.log("lastArrived = " + lastArrived);
+    console.log("minAwahy = " + minAway);
+    console.log("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
+
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trName),
+        $("<td>").text(trDestination),
+        $("<td>").text(trFrequency),
+        $("<td>").text(trFrequency),
+        $("<td>").text(trFrequency),
+    );
+
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+
+
+
     // alert("Train successfully added");
     // console.log("buton clicked");
     // Clears all of the text-boxes
@@ -67,33 +135,3 @@ $("#addTrain-btn").on("click", function(event) {
 
 
 });
-
-database.ref().on("child_added", function(childSnapshot) {
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-    console.log(childSnapshot.val());
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-    // Store everything into a variable.
-    var trNam = childSnapshot.val().name
-    var trdest = childSnapshot.val().destination;
-    var trfreq = childSnapshot.val().frequency;
-    var trFirstT = childSnapshot.val().firstTime;
-
-
-
-    console.log("::::::::::::::::::::::::::::::::::::");
-    console.log(trNam);
-    console.log(trdest);
-    console.log(trfreq);
-    console.log(trFirstT);
-    console.log("::::::::::::::::::::::::::::::::::::");
-
-    //Calculate train timint
-    var initialTrTime = moment(trFirstT, "HH:mm ");
-    console.log("::::::::::::::::::::::::::::::::::::");
-    console.log(trFirstT);
-    console.log("::::::::::::::::::::::::::::::::::::");
-    console.log("____________________________________");
-    console.log(initialTrTime);
-    console.log("____________________________________");
-})
